0d07d0f9784976dcf26bd271621b6a2c4af25452
Maegan Wilson <maegan@maeganwilson.com>
2018-04-17 21:41:51 -0500

    Share icons added

--- src/article/index.js
+++ src/article/index.js
@@ -8,6 +8,17 @@ class Article extends Component {
           <h1>{this.props.title}</h1>
           <section>
             <img src={this.props.urlToImage} alt={this.props.title} />
+            <p className="share">
+              <a href="https://www.facebook.com/saatva/">
+                <i className="fab fa-facebook-square" />
+              </a>
+              <a href="https://twitter.com/SaatvaMattress">
+                <i className="fab fa-twitter-square" />
+              </a>
+              <a href="mailto:">
+                <i className="fas fa-envelope-square" />
+              </a>
+            </p>
           </section>
         </header>
         <main
